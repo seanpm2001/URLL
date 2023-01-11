ccc817e52d204a185063d9bf17d171d422826b8c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-11 15:42:26 -0800

    HTTPS_V1.py
    HTTPS_V1.py

---
+++ Protocols/H/HTTPS/Handlers/OldVersions/Python/HTTPS_V1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# HTTPS handler
+# This script is coming soon
+
+redirect = str("http://")
+redirect1 = bool(False)
+
+# File version: 1 (2023, Wednesday, January 11th at 3:42 pm PST)
