dbd6cbfc6e2e961400549354470a4e28f44613b7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-11 15:39:03 -0800

    HTTP.py [V1]
    Version 1 of the HTTP handler file for this project.

---
+++ Protocols/H/HTTP/Handlers/HTTP.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# HTTP handler
+# This script is coming soon
+
+redirect = str("https://")
+redirect1 = bool(False)
+
+# File version: 1 (2023, Wednesday, January 11th at 3:39 pm PST)
